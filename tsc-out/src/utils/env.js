be07554264a2b2e666594310c83f2aa3f51e274b
HerbLuo <cloudself.cn@gmail.com>
2018-10-26 18:03:18 +0800

    tsc-out removed

--- tsc-out/src/utils/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-exports.isDebug = process.env.NODE_ENV !== 'production';
-//# sourceMappingURL=env.js.map
